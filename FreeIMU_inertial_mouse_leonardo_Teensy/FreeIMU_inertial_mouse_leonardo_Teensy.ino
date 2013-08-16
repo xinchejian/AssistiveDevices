cf2b36a7a8b04d5b57a809325d05fa84edfd473f
spanner888 <spanner888@xinchejian.com>
2013-08-16 13:14:26 +0800

    ... and here are the reset commands + notes about status
    status - was trying to implement "easy" for user way to swtich between
    hardware
    .. currently have challenge getting example #def approach to work

--- FreeIMU_inertial_mouse_leonardo_Teensy/FreeIMU_inertial_mouse_leonardo_Teensy.ino
+++ FreeIMU_inertial_mouse_leonardo_Teensy/FreeIMU_inertial_mouse_leonardo_Teensy.ino
@@ -13,7 +13,19 @@ The best way to do this is wipe out your working tree (all the files except the 
 ****Make sure you've committed any work before you do this, or it will be lost.
 
 
+$ git rm --cached -r .
+# Remove everything from the index.
 
+$ git reset --hard
+# Write both the index and working directory from git's database.
+
+$ git add .
+# Prepare to make a commit by staging all the files that will get normalized.
+# This is your chance to inspect which files were never normalized. You should
+# get lots of messages like: "warning: CRLF will be replaced by LF in file."
+
+$ git commit -m "Normalize line endings"
+# Commit
 */
 
 /*
